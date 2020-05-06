36eca68cf9969264505457810b29960a9c59b107
Aaron Timbol <timbolaj@gmail.com>
2020-05-06 18:59:44 +0000

    ensured that ctrl + c works

--- play.js
+++ play.js
@@ -1,5 +1,24 @@
 //IP Address 192.168.1.88
 //Port 50541
+const setUpInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  const handleUserInput = key => {
+    if (key === '\u0003') {
+      process.exit()
+    }
+  }
+  
+  stdin.on('data', (data) => {
+    handleUserInput(data);
+  }) 
+  return stdin;
+}
+
 const { connect } = require('./client');
 console.log('Connecting ...');
+setUpInput();
 connect()
